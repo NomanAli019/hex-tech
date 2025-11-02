9e4afdfa49aff02dd59dbaa3c8c5d608f69cd88f
NomanAli019 <nomanromeo694.com>
2025-11-02 18:14:47 +0500

    compasdlkf

--- attached_assets/App_1762048489687.tsx
+++ attached_assets/App_1762048489687.tsx
@@ -1,6 +1,39 @@
+// import { Switch, Route } from "wouter";
+// import { queryClient } from "./lib/queryClient";
+// import { QueryClientProvider } from "@tanstack/react-query";
+// import { Toaster } from "@/components/ui/toaster";
+// import { TooltipProvider } from "@/components/ui/tooltip";
+// import Home from "@/pages/Home";
+// import WhatWeDo from "@/pages/WhatWeDo";
+// import Projects from "@/pages/Projects";
+// import Team from "@/pages/Team";
+// import NotFound from "@/pages/not-found";
+
+// function Router() {
+//   return (
+//     <Switch>
+//       <Route path="/" component={Home} />
+//       <Route path="/what-we-do" component={WhatWeDo} />
+//       <Route path="/projects" component={Projects} />
+//       <Route path="/team" component={Team} />
+//       <Route component={NotFound} />
+//     </Switch>
+//   );
+// }
+
+// function App() {
+//   return (
+//     <QueryClientProvider client={queryClient}>
+//       <TooltipProvider>
+//         <Toaster />
+//         <Router />
+//       </TooltipProvider>
+//     </QueryClientProvider>
+//   );
+// }
+
+// export default App;
 import { Switch, Route } from "wouter";
-import { queryClient } from "./lib/queryClient";
-import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import Home from "@/pages/Home";
@@ -23,12 +56,10 @@ function Router() {
 
 function App() {
   return (
-    <QueryClientProvider client={queryClient}>
-      <TooltipProvider>
-        <Toaster />
-        <Router />
-      </TooltipProvider>
-    </QueryClientProvider>
+    <TooltipProvider>
+      <Toaster />
+      <Router />
+    </TooltipProvider>
   );
 }
 
